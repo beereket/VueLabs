2cd431acbbb47e6b27bc9b7cac897e762e36ebb8
yergalibereket <b_yergali@outlook.com>
2024-09-16 17:28:05 +0500

    Lab1 finished!

---
+++ Lab1/my-vue/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <!--  <img alt="Vue logo" src="./assets/logo.png">-->
+  <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <Header></Header>
+  <Main></Main>
+</template>
+
+<script>
+
+import Main from "@/components/Main.vue";
+import Header from "@/components/Header.vue";
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Main,
+    Header,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ Lab1/my-vue/src/components/Main.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <div class="image-container">
+      <div class="image-item">
+        <img src="../assets/mha.jpg" alt="image" class="image" width="480">
+        <p class="text">Midoriya's on a mission to push beyond his limits!</p>
+      </div>
+
+      <div class="image-item">
+        <img src="../assets/gumble.jpg" alt="image" class="image" width="480">
+        <p class="text">Gumball, ready to crush it!</p>
+      </div>
+
+      <div class="image-item">
+        <img src="../assets/batman.jpg" alt="image" class="image" width="480" height="800">
+        <p class="text">Even heroes need strength training</p>
+      </div>
+
+      <div class="image-item">
+        <img src="../assets/goku.jpg" alt="image" class="image" width="480">
+        <p class="text">Goku never skips leg day!</p>
+      </div>
+
+      <div class="image-item">
+        <img src="../assets/darwin.jpg" alt="image" class="image" width="480">
+        <p class="text">Watch out, he's about to go Super Saiyan!</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: `Main`
+}
+</script>
+
+<style scoped>
+.container {
+  padding: 2rem 5%;
+}
+
+.image-container {
+  columns: 15rem 3;
+  gap: 1.2rem;
+}
+
+.image-item {
+  position: relative;
+  margin-bottom: 1rem;
+}
+
+.image {
+  width: 100%;
+  border-radius: 0.7rem;
+}
+
+.text {
+  position: absolute;
+  bottom: 10px;
+  left: 40%;
+  transform: translateX(-50%);
+  color: white;
+  font-size: 20px;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
+  text-align: center;
+}
+
+.image-item:hover {
+  transform: scale(1.01);
+}
+
+
+</style>
